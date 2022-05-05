2353e950d64fa6c033f21336743067ad354e6ea2
DappySorrow <j.si.barbeau8@gmail.com>
2022-05-05 10:17:16 -0400

    Timer Splash commencé

--- TPApps/app/src/main/java/com/tp_apps/presentation/ui/splash/SplashActivity.kt
+++ TPApps/app/src/main/java/com/tp_apps/presentation/ui/splash/SplashActivity.kt
@@ -2,6 +2,8 @@ package com.tp_apps.presentation.ui.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.tp_apps.MainActivity
 import com.tp_apps.R
@@ -11,14 +13,35 @@ class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
 
+    var counter = 0
+
+    val timer = object: CountDownTimer(10000, 1000) {
+        override fun onTick(millisUntilFinished: Long) {
+            binding.txvCounter.text = (++counter).toString()
+            binding.pgbLoading.setProgress(counter, true)
+        }
+
+        override fun onFinish() {
+            startActivity(MainActivity.newIntent(this@SplashActivity))
+            this@SplashActivity.finish()
+        }
+    }
+
+    //==========================================================================================
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        timer.start()
+
         binding.test.setOnClickListener() {
+            timer.cancel()
             startActivity(MainActivity.newIntent(this))
             this.finish()
         }
+
+
     }
 }
