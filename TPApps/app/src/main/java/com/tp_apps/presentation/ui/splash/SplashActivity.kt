81329ae4372baf7c71b84072d30b14e94f540213
DappySorrow <j.si.barbeau8@gmail.com>
2022-05-05 11:52:20 -0400

    Splash Activity done

--- TPApps/app/src/main/java/com/tp_apps/presentation/ui/splash/SplashActivity.kt
+++ TPApps/app/src/main/java/com/tp_apps/presentation/ui/splash/SplashActivity.kt
@@ -1,31 +1,26 @@
 package com.tp_apps.presentation.ui.splash
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.viewbinding.library.activity.viewBinding
+import android.viewbinding.library.fragment.viewBinding
+import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.tp_apps.MainActivity
 import com.tp_apps.R
 import com.tp_apps.core.Constants.TIMER_INTERVAL
 import com.tp_apps.core.Constants.TIMER_MAX
 import com.tp_apps.databinding.ActivitySplashBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
-
-    var counter = 0
-
-    private val timer = object: CountDownTimer(TIMER_MAX, TIMER_INTERVAL) {
-        override fun onTick(millisUntilFinished: Long) {
-            binding.txvCounter.text = getString(R.string.timer, (++counter).toString())
-            binding.pgbLoading.setProgress(counter, true)
-        }
-
-        override fun onFinish() {
-            startActivity(MainActivity.newIntent(this@SplashActivity))
-            this@SplashActivity.finish()
-        }
-    }
+    private val viewModel: SplashViewModel by viewModels()
 
     //==========================================================================================
 
@@ -34,14 +29,26 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        timer.start()
+        viewModel.counter.observe(this) {
+            binding.txvCounter.text = getString(R.string.timer, (it).toString())
+            binding.pgbLoading.setProgress(it, true)
+        }
 
+        viewModel.isTimerDone.observe(this) {
+            if (it) {
+                startActivity(MainActivity.newIntent(this))
+                this.finish()
+            }
+        }
+
+
+/*
         binding.test.setOnClickListener() {
             timer.cancel()
             startActivity(MainActivity.newIntent(this))
             this.finish()
         }
-
+*/
 
     }
 }
