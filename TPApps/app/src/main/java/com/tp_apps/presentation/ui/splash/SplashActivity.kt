ab7a70695ea58b228eb1eb7587d6d52d18b96db7
DappySorrow <j.si.barbeau8@gmail.com>
2022-05-05 11:56:30 -0400

    Optimisation des imports

--- TPApps/app/src/main/java/com/tp_apps/presentation/ui/splash/SplashActivity.kt
+++ TPApps/app/src/main/java/com/tp_apps/presentation/ui/splash/SplashActivity.kt
@@ -1,19 +1,11 @@
 package com.tp_apps.presentation.ui.splash
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.viewbinding.library.activity.viewBinding
-import android.viewbinding.library.fragment.viewBinding
 import androidx.activity.viewModels
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
+import androidx.appcompat.app.AppCompatActivity
 import com.tp_apps.MainActivity
 import com.tp_apps.R
-import com.tp_apps.core.Constants.TIMER_INTERVAL
-import com.tp_apps.core.Constants.TIMER_MAX
 import com.tp_apps.databinding.ActivitySplashBinding
 
 @SuppressLint("CustomSplashScreen")
@@ -41,14 +33,10 @@ class SplashActivity : AppCompatActivity() {
             }
         }
 
-
-/*
+        //Pour permettre aux collegues de skiper
         binding.test.setOnClickListener() {
-            timer.cancel()
-            startActivity(MainActivity.newIntent(this))
             this.finish()
+            startActivity(MainActivity.newIntent(this))
         }
-*/
-
     }
 }
