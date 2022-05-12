529e57c0f0c03b65c66c184d673827d30b9f0680
Benjamin Lemire <benlemire@hotmail.ca>
2022-05-12 16:01:15 -0400

    correction du refresh des tickets

--- TPApps/app/src/main/java/com/tp_apps/helpers/Constants.kt
+++ TPApps/app/src/main/java/com/tp_apps/helpers/Constants.kt
@@ -14,7 +14,7 @@ object Constants {
     }
     const val TIMER_MAX = 10000L
     const val TIMER_INTERVAL = 1000L
-    const val REFRESH_TICKET_DELAY = 1000L
+    const val REFRESH_TICKET_DELAY = 30000L
     const val REFRESH_NETWORK_DELAY = 120000L
 
     const val FLAG_API_URL = "https://flagcdn.com/h40/%s.png"
