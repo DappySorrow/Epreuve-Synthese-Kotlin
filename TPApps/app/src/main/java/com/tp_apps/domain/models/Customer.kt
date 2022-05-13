285418064ca43237b91365a7497619c4dda2a4ee
olivier <1847622@cstj.qc.ca>
2022-05-12 22:23:01 -0700

    Ajout des gateways sur les tickets

--- TPApps/app/src/main/java/com/tp_apps/domain/models/Customer.kt
+++ TPApps/app/src/main/java/com/tp_apps/domain/models/Customer.kt
@@ -13,5 +13,6 @@ data class Customer(
     val city: String = "",
     val country: String = "",
     val postalCode: String = "",
-    val phone: String = ""
+    val phone: String = "",
+    val listGateway : List<Gateway> = listOf()
 )
